a89cf9161f17dce031afa94a42a651d56ad5c85c
Ronaldo Andrianandrasana <ronaldop16.aps1@gmail.com>
2020-09-15 01:49:11 +0400

    Splash screen

---
+++ FreeDemy/app/src/main/java/com/master/mobile/freedemy/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package com.master.mobile.freedemy;
+
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        /*checkLoginStatus();
+        View view = findViewById(R.id.logo);
+        Utils.setLogo(view);*/
+    }
+
+    public SharedPreferences getPreferences() {
+        return preferences;
+    }
+
+    public void setPreferences(SharedPreferences preferences) {
+        this.preferences = preferences;
+    }
+}
---
+++ FreeDemy/app/src/main/java/com/master/mobile/freedemy/utils/Utils.java
@@ -0,0 +1,8 @@
+package com.master.mobile.freedemy.utils;
+
+import android.view.View;
+import android.widget.ImageView;
+
+public class Utils {
+
+}
